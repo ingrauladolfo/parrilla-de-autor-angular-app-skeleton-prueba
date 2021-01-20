5f2141921cf854e6acc6e64a6107c1111275bb67
Ing Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-01-19 18:49:44 -0600

    Finish demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent, pathMatch: 'full'},
+  {path:'nuestro-menu', component:MenuComponent},
+  {path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
